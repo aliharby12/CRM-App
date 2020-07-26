8bc004b93e382fb794d980bf98f0255cab31779a
aliharby12 <aliharby565@gmail.com>
2020-07-26 19:15:55 +0200

    add user field to every single customer

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
+from django.http import HttpResponse
 from .forms import OrderForm, UserRegisterForm
 from django.forms import inlineformset_factory
 from .filters import OrderFilter
@@ -7,10 +8,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import Group
-from .decorators import unauthenticated_user, allowed_users, admin_only
 
 @login_required(login_url='login')
-@admin_only
 def home(request):
     """return the dashboard templates"""
     orders = Order.objects.all()
@@ -35,14 +34,12 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 @login_required(login_url = 'login')
-@allowed_users(allowed_roles=['admin'])
 def products(request):
     """return the products templates"""
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products':products})
 
 @login_required(login_url = 'login')
-@allowed_users(allowed_roles=['admin'])
 def customer(request, id):
     """return the customer detail"""
     customer = Customer.objects.get(id=id)
@@ -53,7 +50,6 @@ def customer(request, id):
     return render(request, 'accounts/customer.html', context)
 
 @login_required(login_url = 'login')
-@allowed_users(allowed_roles=['admin'])
 def createOrder(request, id):
     """create a view to order create"""
     OrderFormSet = inlineformset_factory(Customer, Order, fields=('product', 'status'), extra=10 )
@@ -69,7 +65,6 @@ def createOrder(request, id):
     return render(request, 'accounts/order_form.html', context)
 
 @login_required(login_url = 'login')
-@allowed_users(allowed_roles=['admin'])
 def updateOrder(request, id):
     """create a function to edit specific order"""
     order = Order.objects.get(id=id)
@@ -84,7 +79,6 @@ def updateOrder(request, id):
     return render(request, 'accounts/order_form.html', context)
 
 @login_required(login_url = 'login')
-@allowed_users(allowed_roles=['admin'])
 def deleteOrder(request, id):
     """create a function to delete specific order"""
     order = Order.objects.get(id=id)
@@ -95,7 +89,6 @@ def deleteOrder(request, id):
     context = {'order':order}
     return render(request, 'accounts/delete.html', context)
 
-@unauthenticated_user
 def userRegister(request):
     """user registration view"""
     form = UserRegisterForm()
@@ -113,7 +106,7 @@ def userRegister(request):
     context = {'form':form}
     return render(request, 'accounts/register.html', context)
 
-@unauthenticated_user
+
 def userLogin(request):
     """user login view"""
     if request.method == 'POST':
@@ -128,7 +121,7 @@ def userLogin(request):
         if user is not None:
             login(request, user)
             messages.success(request, 'congratulations, you are now logged in !')
-            return redirect('dashboard')
+            return redirect('user-page')
         else:
             messages.info(request, 'Username or password are incorrect')
     return render(request, 'accounts/login.html', {})
@@ -139,7 +132,20 @@ def userLogout(request):
     messages.success(request, 'now, you are logged out !')
     return redirect('login')
 
+@login_required(login_url = 'login')
 def userPage(request):
     """create a user page to render user information"""
-    context = {}
+    orders = request.user.customer.order_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    out_of_delivery = orders.filter(status='Out of delivery').count()
+
+    context = {
+        'orders':orders,
+        'total_orders':total_orders,
+        'delivered':delivered,
+        'pending':pending,
+        'out_of_delivery':out_of_delivery
+    }
     return render(request, 'accounts/user.html', context)
